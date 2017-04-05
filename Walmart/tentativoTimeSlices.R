c42f85dc2172fbafbdabdb66585248e43b71dd12
CarioniStefano <phoenixxperia@gmail.com>
2017-04-05 12:31:23 +0200

    generalizzato per lista store e lista reparti

--- Walmart/tentativoTimeSlices.R
+++ Walmart/tentativoTimeSlices.R
@@ -59,12 +59,12 @@ options(scipen = 999)
 #     index.column = 3,
 #     sep = ","
 #   )
-stores <- read_csv("~/Lavoro/InputFiles/stores.csv")
-
-notZoo <- read.csv( file = "~/Lavoro/InputFiles/train.csv", header = TRUE, sep = ",")
-
-notZoo <- xts(notZoo , order.by=make.time.unique(as.POSIXct(notZoo[,3]) ))
-
+# stores <- read_csv("~/Lavoro/InputFiles/stores.csv")
+# 
+# notZoo <- read.csv( file = "~/Lavoro/InputFiles/train.csv", header = TRUE, sep = ",")
+# 
+# notZoo <- xts(notZoo , order.by=make.time.unique(as.POSIXct(notZoo[,3]) ))
+# 
 
 # dataNumber <- table(trainFile.z$Store)
 
@@ -88,144 +88,146 @@ notZoo <- xts(notZoo , order.by=make.time.unique(as.POSIXct(notZoo[,3]) ))
 storesNumbers <- c(1,2,4,10,13,24)
 storesNumbersSecond <- c(1,2,4,10,13,24)
 deptNumbers <- c(21,81,91,40,7,92)
-deptNumbers <- c(21)
+# deptNumbers <- c(21)
 
 deptDivisionList <- list()
-# par(mfrow = c(3, 2))
+par(mfrow = c(3, 2))
 for(deptSelected in deptNumbers){
-
-  print(deptSelected)}
-deptDivisionList <- list(deptDivisionList,list(newObj))
-
-
-
-
-storesBinded.z = notZoo[which(as.numeric(notZoo$Store) == head(storesNumbers,1)), ]
-
-storesBinded.z <- storesBinded.z[, c(3, 5, 2, 1, 4)]
-
-storesNumbers=tail(storesNumbers,-1)
-
-for(storeNo in storesNumbers){
   
-  print(storeNo)
-  storesBinded.z = cbind(storesBinded.z , notZoo[which(as.numeric(notZoo$Store) == storeNo), c(1,4)])
-  
-}
-
-storesBinded.z = storesBinded.z[which(as.numeric(storesBinded.z$Dept) == 21), ]
-
-from <- as.Date("2010-02-05")
-to <- as.Date("2012-10-26")
-months <- seq.Date(from=from,to=to,by="week")
-
-values <- rep.int(0,length(months))
-
-Zooserie <- zoo(values, months)
-
-storeWindowed <- Zooserie
-storeWindowed <- merge(storeWindowed,Zooserie)
-storeWindowed <- merge(storeWindowed,Zooserie)
-storeWindowed <- merge(storeWindowed,Zooserie)
-
-
-weekNo <- as.POSIXlt(index(storeWindowed))
-storeWindowed[,1] <- as.numeric((strftime(weekNo,format="%Y%m")), sep = "")
-storeWindowed[,2] <- as.numeric(strftime(weekNo,format="%m"))
-storeWindowed[,3] <- as.numeric(strftime(weekNo,format="%Y"))
-storeWindowed[,4] <- as.numeric(storesBinded.z$Dept)
-
-colnames(storeWindowed) <- c("YearMonth","Month","Year","Dept")
-
-
-
-for(storeNo in storesNumbersSecond){
-  
-  # print(storeNo)
-  # print(match(storeNo,storesNumbersSecond))
-  # # rollapply(storesBinded.z[,(3+2*(match(storeNo,storesNumbersSecond)))], width = 4, FUN = mean, align = "left")
-  # print(dim(storeWindowed))
-  # print(dim( rollapply(storesBinded.z[,(3+2*(match(storeNo,storesNumbersSecond)))], width = 4, FUN = mean, align = "left")))
-  
-  storeWindowed = cbind(storeWindowed , rollapply(storesBinded.z[,(3+2*(match(storeNo,storesNumbersSecond)))], width = 4, FUN = mean, align = "left"))
+  print(deptSelected)
+  # deptDivisionList <- list(deptDivisionList,list(newObj))
   
   
-}
-
-storeWindowed<-storeWindowed[!duplicated(storeWindowed[,1]),]
-
-for(storeNo in storesNumbersSecond){
   
-  storeWindowed[,(4+(match(storeNo,storesNumbersSecond)))] <- (rescale(storeWindowed[,(4+(match(storeNo,storesNumbersSecond)))],to=c(0,1)))
   
-}
-
-
-
-years <- c(2010,2011,2012)#numero di reparti selezionati
-namedepts <-
-  c("2010","2011","2012"  )
-
-
-colIndex <- 1
-
-colors = rainbow(ncol(storeWindowed))
-# colors <-
-#   c("coral1",
-#     "chartreuse3",
-#     "chocolate4",
-#     "orange",
-#     "black",
-#     "purple","royalblue","seagreen4","violetred1")
-
-# get the range for the x and y axis
-xrange <- range(0,12)
-yrange <- range(c(0,1))
-# print(yrange)
-# print(xrange)
-plot(xrange,
-     yrange,
-     type = "n",
-     xlab = "Date",
-     ylab = "WeeklySales")
-
-
-
-
-
-linetype <- 1
-pchDot <- 16
-
-
-for (j in years) {
+  storesBinded.z = notZoo[which(as.numeric(notZoo$Store) == head(storesNumbers,1)), ]
   
-  print(j)
+  storesBinded.z <- storesBinded.z[, c(3, 5, 2, 1, 4)]
+  storesNumbers <- c(1,2,4,10,13,24)
+  storesNumbers=tail(storesNumbers,-1)
   
-  tempDept <- data.frame( subset(storeWindowed, Year == j))
-  
-  for(rowCount in 5:ncol(tempDept)){
+  for(storeNo in storesNumbers){
     
-    print(rowCount)
+    print(storeNo)
+    storesBinded.z = cbind(storesBinded.z , notZoo[which(as.numeric(notZoo$Store) == storeNo), c(1,4)])
     
-    lines(tempDept$Month,tempDept[,rowCount],
-          lty = linetype,
-          lwd = 2,
-          col = colors[colIndex],
-          pch = pchDot
-    )
-    colIndex <- colIndex + 1
   }
-  # legend(
-  #   xrange[1],
-  #   yrange[2],
-  #   namedepts,
-  #   cex = 1,
-  #   col = colors,
-  #   pch = pchDot,
-  #   lty = linetype,
-  #   title = "Dept"
-  # )
+  
+  storesBinded.z = storesBinded.z[which(as.numeric(storesBinded.z$Dept) == deptSelected), ]
+  
+  from <- as.Date("2010-02-05")
+  to <- as.Date("2012-10-26")
+  months <- seq.Date(from=from,to=to,by="week")
+  
+  values <- rep.int(0,length(months))
+  
+  Zooserie <- zoo(values, months)
+  
+  storeWindowed <- Zooserie
+  storeWindowed <- merge(storeWindowed,Zooserie)
+  storeWindowed <- merge(storeWindowed,Zooserie)
+  storeWindowed <- merge(storeWindowed,Zooserie)
   
   
+  weekNo <- as.POSIXlt(index(storeWindowed))
+  storeWindowed[,1] <- as.numeric((strftime(weekNo,format="%Y%m")), sep = "")
+  storeWindowed[,2] <- as.numeric(strftime(weekNo,format="%m"))
+  storeWindowed[,3] <- as.numeric(strftime(weekNo,format="%Y"))
+  storeWindowed[,4] <- as.numeric(storesBinded.z$Dept)
+  
+  colnames(storeWindowed) <- c("YearMonth","Month","Year","Dept")
+  
+  
+  
+  for(storeNo in storesNumbersSecond){
+    
+    # print(storeNo)
+    # print(match(storeNo,storesNumbersSecond))
+    # # rollapply(storesBinded.z[,(3+2*(match(storeNo,storesNumbersSecond)))], width = 4, FUN = mean, align = "left")
+    # print(dim(storeWindowed))
+    # print(dim( rollapply(storesBinded.z[,(3+2*(match(storeNo,storesNumbersSecond)))], width = 4, FUN = mean, align = "left")))
+    
+    storeWindowed = cbind(storeWindowed , rollapply(storesBinded.z[,(3+2*(match(storeNo,storesNumbersSecond)))], width = 4, FUN = mean, align = "left"))
+    
+    
+  }
+  
+  storeWindowed<-storeWindowed[!duplicated(storeWindowed[,1]),]
+  
+  for(storeNo in storesNumbersSecond){
+    
+    storeWindowed[,(4+(match(storeNo,storesNumbersSecond)))] <- (rescale(storeWindowed[,(4+(match(storeNo,storesNumbersSecond)))],to=c(0,1)))
+    
+  }
+  
+  
+  
+  years <- c(2010,2011,2012)#numero di reparti selezionati
+  namedepts <-
+    c("2010","2011","2012"  )
+  
+  
+  colIndex <- 1
+  
+  colors = rainbow(ncol(storeWindowed))
+  # colors <-
+  #   c("coral1",
+  #     "chartreuse3",
+  #     "chocolate4",
+  #     "orange",
+  #     "black",
+  #     "purple","royalblue","seagreen4","violetred1")
+  
+  # get the range for the x and y axis
+  xrange <- range(0,12)
+  yrange <- range(c(0,1))
+  # print(yrange)
+  # print(xrange)
+  plot(xrange,
+       yrange,
+       type = "n",
+       xlab = "Date",
+       ylab = "WeeklySales")
+  
+  
+  
+  
+  
+  linetype <- 1
+  pchDot <- 16
+  
+  
+  for (j in years) {
+    
+    print(j)
+    
+    tempDept <- data.frame( subset(storeWindowed, Year == j))
+    
+    for(rowCount in 5:ncol(tempDept)){
+      
+      print(rowCount)
+      
+      lines(tempDept$Month,tempDept[,rowCount],
+            lty = linetype,
+            lwd = 2,
+            col = colors[colIndex],
+            pch = pchDot
+      )
+      colIndex <- colIndex + 1
+    }
+    # legend(
+    #   xrange[1],
+    #   yrange[2],
+    #   namedepts,
+    #   cex = 1,
+    #   col = colors,
+    #   pch = pchDot,
+    #   lty = linetype,
+    #   title = "Dept"
+    # )
+    
+    
+  }
 }
+
 
